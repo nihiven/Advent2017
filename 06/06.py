dd1ecec525f5d4ebb3f801d8ea3860540857d206
Justin Behanna <j.behanna@gmail.com>
2017-12-08 15:32:05 -0500

    working on 0701

--- 06/06.py
+++ 06/06.py
@@ -2,4 +2,32 @@ f = open('example.txt', 'r')
 inputData = f.read()
 f.close()
 
-line = inputData.split('\n')
+
+class Node():
+    def __init__(self, name):
+        self.name = name
+        self.children = {}
+        self.parent = None
+
+    def addChild(self, child):
+        child.setParent = self
+        self.children.append(child)
+
+    def setParent(self, parent):
+        self.parent = parent
+
+
+lines = inputData.split('\n')
+
+blocks = {}
+for line in lines:
+    parts = line.split(',')
+    name = parts[0]
+    final = ''.join(parts).split(' ')
+    if len(final) < 3:
+        continue
+    print final
+
+
+
+print blocks
